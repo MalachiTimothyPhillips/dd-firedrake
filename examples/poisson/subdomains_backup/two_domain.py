2fb2cc1c13c553723ef49bd748b8c57e3f23b1d4
MalachiTimothyPhillips <malachiphillips@ou.edu>
2018-12-04 15:28:38 -0600

    Fix subdomain integration issue by employing the heaviside function Dirichlet B.C. hack

--- examples/poisson/subdomains_backup/two_domain.py
+++ examples/poisson/subdomains_backup/two_domain.py
@@ -15,6 +15,7 @@ We are confident that we must have the correct solution for this particular prob
 """
 
 from firedrake import *
+import numpy as np
 mesh=Mesh("TwoDomain.msh")
 V=FunctionSpace(mesh, "CG", 1) # piecewise linear elements
 
@@ -30,14 +31,43 @@ f=Function(V)
 x,y=SpatialCoordinate(mesh)
 f.interpolate((8*pi*pi)*sin(2*pi*x)*sin(2*pi*y))
 
-# TODO: Need to be able to do this integral just over
-# omega1 or just over omega2 -- this probably means that
-# I need to enforce a 0 dirichlet boundary condition on
-# all points that are outside of this domain!!
-a1 = (dot(grad(u), grad(v))) * dx
+V_DG0_O1 = FunctionSpace(mesh, "DG", 0)
+V_DG0_O2 = FunctionSpace(mesh, "DG", 0)
+
+# Heaviside step function in omega 1
+I_O1 = Function( V_DG0_O1 )
+par_loop( 'for ( int i=0; i < f.dofs; i++ ) f[i][0] = 1.0;', dx(omega1_id), {'f': (I_O1, WRITE)} )
+I_cg_O1 = Function(V)
+par_loop( 'for (int i=0; i<A.dofs; i++) A[i][0] = fmax(A[i][0], B[0][0]);',
+         dx, {'A' : (I_cg_O1, RW), 'B': (I_O1, READ)} )
+
+# Heaviside step function in omega 2
+I_O2 = Function( V_DG0_O2 )
+par_loop( 'for ( int i=0; i < f.dofs; i++ ) f[i][0] = 1.;', dx(omega2_id), {'f': (I_O2, WRITE)} )
+I_cg_O2 = Function(V)
+par_loop( ' for (int i=0; i<A.dofs; i++) for(int j=0; j<2; j++) A[i][j] = fmax(A[i][j], B[0][0]);',
+         dx, {'A' : (I_cg_O2, RW), 'B': (I_O2, READ)} )
+
+
+class MyBC(DirichletBC):
+    def __init__(self, V, value, markers):
+        # Call superclass init
+        # We provide a dummy subdomain id.
+        super(MyBC, self).__init__(V, value, 0)
+        # Override the "nodes" property which says where the boundary
+        # condition is to be applied.
+        self.nodes = np.unique(np.where(markers.dat.data_ro_with_halos == 0)[0])
+
+# magical B.C. generator to allow us not to have singular system
+BC_include_O2_only = MyBC(V,0,I_cg_O2)
+BC_include_O1_only = MyBC(V,0,I_cg_O1)
+
+
+
+a1 = (dot(grad(u), grad(v))) * dx(omega1_id)
 L1 = f * v * dx(omega1_id)
-a2 = (dot(grad(v), grad(u))) * dx
-L2 = f * v * dx
+a2 = (dot(grad(v), grad(u))) * dx(omega2_id)
+L2 = f * v * dx(omega2_id)
 
 u1=Function(V)
 u2=Function(V)
@@ -77,8 +107,8 @@ nSchwarzIter=100
 params={'ksp_type':'preonly','pc_type':'lu'}
 
 for iteration in range(nSchwarzIter):
-    solve(a1 == L1,u1,bcs=[bc1,bc2,bc3,bc4,bc5,bc6,bc_dOmega1_n_Omega2], solver_parameters=params)
-    solve(a2 == L2,u2,bcs=[bc1,bc2,bc3,bc4,bc5,bc6,bc_dOmega2_n_Omega1], solver_parameters=params)
+    solve(a1 == L1,u1,bcs=[bc1,bc2,bc3,bc4,bc5,bc6,bc_dOmega1_n_Omega2,BC_include_O1_only], solver_parameters=params)
+    solve(a2 == L2,u2,bcs=[bc1,bc2,bc3,bc4,bc5,bc6,bc_dOmega2_n_Omega1,BC_include_O2_only], solver_parameters=params)
 
 e1=assemble(dot(u1-f,u1-f)*dx(omega1_id))
 e2=assemble(dot(u2-f,u2-f)*dx(omega2_id))
