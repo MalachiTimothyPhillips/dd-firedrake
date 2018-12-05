f66799df1624cb2b5aac932c0ced7651de8fe446
MalachiTimothyPhillips <malachiphillips@ou.edu>
2018-12-05 00:36:03 -0600

    Two points:
    1) Test out vim greek characters in python -- generally a success, however, to painstaking to actually type out
    
    2) Implement the 2x2 overlapping case. Hopefully this should provide enough insight to work on high dimensionality
    cases, too

--- examples/poisson/four_domain/four_domain.py
+++ examples/poisson/four_domain/four_domain.py
@@ -15,38 +15,62 @@ We are confident that we must have the correct solution for this particular prob
 """
 
 from firedrake import *
+import numpy as np
 mesh=Mesh("2x2Overlap.msh")
 V=FunctionSpace(mesh, "CG", 1) # piecewise linear elements
 
-omega1_id=1
-omega2_id=2
-omega3_id=3
-omega4_id=4
+# Boundary identities
+O1=1
+O2=2
+O3=3
+O4=4
 
-omega2_face_id=2
-omega1_face_id=3
+# Interface identities
+dO1nO2=4
+dO1nO3=3
+dO2nO1=6
+dO2nO4=7
+dO3nO1=9
+dO3nO4=12
+dO4nO2=13
+dO4nO3=14
+
 
 u=TrialFunction(V)
 v=TestFunction(V)
 f=Function(V)
 x,y=SpatialCoordinate(mesh)
 f.interpolate((8*pi*pi)*sin(2*pi*x)*sin(2*pi*y))
-a1 = (dot(grad(v), grad(u))) * dx
-L1 = f * v * dx(omega1_id)
-a2 = (dot(grad(v), grad(u))) * dx
-L2 = f * v * dx(omega2_id)
 
-u1=Function(V)
-u2=Function(V)
-u1.interpolate(0*x*y)
-u2.interpolate(0*x*y)
+hO1 = FunctionSpace(mesh, "DG", 0)
+hO2 = FunctionSpace(mesh, "DG", 0)
+hO3 = FunctionSpace(mesh, "DG", 0)
+hO4 = FunctionSpace(mesh, "DG", 0)
 
-"""
 
-Homogeneous dirichlet boundary conditions throughout mean that faces 1,2,3,4 must
-have zero on their own boundary conditions
 
-"""
+I_O1 = Function( hO1 )
+par_loop( 'for ( int i=0; i < f.dofs; i++ ) f[i][0] = 1.0;', dx(O1), {'f': (I_O1, WRITE)} )
+I_cg_O1 = Function(V)
+par_loop( 'for (int i=0; i<A.dofs; i++) A[i][0] = fmax(A[i][0], B[0][0]);',
+         dx, {'A' : (I_cg_O1, RW), 'B': (I_O1, READ)} )
+
+I_O2 = Function( hO2 )
+par_loop( 'for ( int i=0; i < f.dofs; i++ ) f[i][0] = 1.;', dx(O2), {'f': (I_O2, WRITE)} )
+I_cg_O2 = Function(V)
+par_loop( ' for (int i=0; i<A.dofs; i++) for(int j=0; j<2; j++) A[i][j] = fmax(A[i][j], B[0][0]);',
+         dx, {'A' : (I_cg_O2, RW), 'B': (I_O2, READ)} )
+
+I_O3 = Function( hO3 )
+par_loop( 'for ( int i=0; i < f.dofs; i++ ) f[i][0] = 1.;', dx(O3), {'f': (I_O3, WRITE)} )
+I_cg_O3 = Function(V)
+par_loop( ' for (int i=0; i<A.dofs; i++) for(int j=0; j<2; j++) A[i][j] = fmax(A[i][j], B[0][0]);',
+         dx, {'A' : (I_cg_O3, RW), 'B': (I_O3, READ)} )
+I_O4 = Function( hO4 )
+par_loop( 'for ( int i=0; i < f.dofs; i++ ) f[i][0] = 1.;', dx(O4), {'f': (I_O4, WRITE)} )
+I_cg_O4 = Function(V)
+par_loop( ' for (int i=0; i<A.dofs; i++) for(int j=0; j<2; j++) A[i][j] = fmax(A[i][j], B[0][0]);',
+         dx, {'A' : (I_cg_O4, RW), 'B': (I_O4, READ)} )
 
 class MyBC(DirichletBC):
     def __init__(self, V, value, markers):
@@ -57,36 +81,98 @@ class MyBC(DirichletBC):
         # condition is to be applied.
         self.nodes = np.unique(np.where(markers.dat.data_ro_with_halos == 0)[0])
 
-x=SpatialCoordinate(mesh)
-bc1=DirichletBC(V,0,1)
-bc2=DirichletBC(V,0,4)
-bc3=DirichletBC(V,0,5)
-bc4=DirichletBC(V,0,6)
-bc5=DirichletBC(V,0,7)
-bc6=DirichletBC(V,0,8)
+# magical B.C. generator to allow us not to have singular system
+BC_O1_only = MyBC(V,0,I_cg_O1)
+BC_O2_only = MyBC(V,0,I_cg_O2)
+BC_O3_only = MyBC(V,0,I_cg_O3)
+BC_O4_only = MyBC(V,0,I_cg_O4)
 
-bc=DirichletBC(V,1,omega1_face_id)
-q=Function(V)
-q=u2
-bc.apply(q)
-bc_dOmega1_n_Omega2=bc
 
-bc=DirichletBC(V,1,omega2_face_id)
-q=Function(V)
-q=u2
-bc.apply(q)
 
-bc_dOmega2_n_Omega1=bc
+a1 = (dot(grad(u), grad(v))) * dx(O1)
+L1 = f * v * dx(O1)
+a2 = (dot(grad(v), grad(u))) * dx(O2)
+L2 = f * v * dx(O2)
+a3 = (dot(grad(v), grad(u))) * dx(O3)
+L3 = f * v * dx(O3)
+a4 = (dot(grad(v), grad(u))) * dx(O4)
+L4 = f * v * dx(O4)
 
-nSchwarzIter=2
+u1=Function(V)
+u2=Function(V)
+u3=Function(V)
+u4=Function(V)
+u1_old=Function(V)
+u2_old=Function(V)
+u3_old=Function(V)
+u4_old=Function(V)
+u1.interpolate(0*x*y)
+u2.interpolate(0*x*y)
+u3.interpolate(0*x*y)
+u4.interpolate(0*x*y)
+u1_old.interpolate(0*x*y)
+u2_old.interpolate(0*x*y)
+u3_old.interpolate(0*x*y)
+u4_old.interpolate(0*x*y)
 
-p_omega_1 = LinearVariationalProblem(a1,L1,u1,bcs=[bc1,bc2,bc3,bc4,bc5,bc6,bc_dOmega1_n_Omega2])
-p_omega_2 = LinearVariationalProblem(a2,L2,u2,bcs=[bc1,bc2,bc3,bc4,bc5,bc6,bc_dOmega2_n_Omega1])
-solver1 = LinearVariationalSolver(p_omega_1)
-solver2 = LinearVariationalSolver(p_omega_2)
 
-#residual_errors=np.zeros(nSchwarzIter+1)
+# zero B.C. on all the exterior lines
+exterior_line_ids=[1,2,10,11,15,16,8,5]
+bcs=[]
+for surface in exterior_line_ids:
+    bcs.append(DirichletBC(V,0.0,surface))
+
+e1=u2_old+u3_old
+e2=u1_old+u4_old
+e3=u1_old+u4_old
+e4=u2_old+u3_old
+
+bc_dO1nO2=DirichletBC(V,e1,dO1nO2)
+bc_dO1nO3=DirichletBC(V,e1,dO1nO3)
+bc1=bcs.copy()
+bc1.append(bc_dO1nO2)
+bc1.append(bc_dO1nO3)
+bc1.append(BC_O1_only)
+
+bc_dO2nO1=DirichletBC(V,e2,dO2nO1)
+bc_dO2nO4=DirichletBC(V,e2,dO2nO4)
+bc2=bcs.copy()
+bc2.append(bc_dO2nO1)
+bc2.append(bc_dO2nO4)
+bc2.append(BC_O2_only)
+
+bc_dO3nO1=DirichletBC(V,e3,dO3nO1)
+bc_dO3nO4=DirichletBC(V,e3,dO3nO4)
+bc3=bcs.copy()
+bc3.append(bc_dO3nO1)
+bc3.append(bc_dO3nO4)
+bc3.append(BC_O3_only)
+
+bc_dO4nO2=DirichletBC(V,e4,dO4nO2)
+bc_dO4nO3=DirichletBC(V,e4,dO4nO3)
+bc4=bcs.copy()
+bc4.append(bc_dO4nO2)
+bc4.append(bc_dO4nO3)
+bc4.append(BC_O4_only)
+
+nSchwarzIter=10
+
+params={'ksp_type':'preonly','pc_type':'lu'}
 
 for iteration in range(nSchwarzIter):
-    solver1.solve()
-    solver2.solve()
+    u1_old=u1
+    u2_old=u2
+    u3_old=u3
+    u4_old=u4
+    solve(a1 == L1,u1,bcs=bc1, solver_parameters=params)
+    solve(a2 == L2,u2,bcs=bc2, solver_parameters=params)
+    solve(a3 == L3,u3,bcs=bc3, solver_parameters=params)
+    solve(a4 == L4,u4,bcs=bc4, solver_parameters=params)
+
+e1=assemble(dot(u1-f,u1-f)*dx(O1))
+e2=assemble(dot(u2-f,u2-f)*dx(O2))
+e3=assemble(dot(u3-f,u3-f)*dx(O3))
+e4=assemble(dot(u4-f,u4-f)*dx(O4))
+
+new_solution_error=sqrt(e1**2+e2**2+e3**2+e4**2)
+print(new_solution_error)
---
+++ examples/poisson/subdomains_backup/two_domain_greek.py
@@ -0,0 +1,116 @@
+"""
+Simple poission problem on the unit square [0,1]^2.
+In particular, we formulate this out of the idea that
+u = sin(2 \pi x) sin (2 \pi u)
+
+Thus we have that \dfrac{\partial^2 u}{\partial x^2} = -4 \pi^2 sin(2\pix)sin(2\piy).
+Similarly, the term \dfrac{\partial^2 u}{\partial y^2} is the same term.
+Notice also that this analytical u respects the boundary condition, and thus with
+the proper forcing function f(x,y):
+
+    f(x,y) = 8\pi^2cos(2\pix)cos(2\piy)
+
+We are confident that we must have the correct solution for this particular problem.
+
+"""
+
+from firedrake import *
+import numpy as np
+mesh=Mesh("TwoDomain.msh")
+V=FunctionSpace(mesh, "CG", 1) # piecewise linear elements
+
+Ω1=1
+Ω2=2
+
+dΩ2nΩ1=2
+dΩ1nΩ2=3
+
+u=TrialFunction(V)
+v=TestFunction(V)
+f=Function(V)
+x,y=SpatialCoordinate(mesh)
+f.interpolate((8*pi*pi)*sin(2*pi*x)*sin(2*pi*y))
+
+V_DG0_O1 = FunctionSpace(mesh, "DG", 0)
+V_DG0_O2 = FunctionSpace(mesh, "DG", 0)
+
+# Heaviside step function in omega 1
+I_O1 = Function( V_DG0_O1 )
+par_loop( 'for ( int i=0; i < f.dofs; i++ ) f[i][0] = 1.0;', dx(Ω1), {'f': (I_O1, WRITE)} )
+I_cg_O1 = Function(V)
+par_loop( 'for (int i=0; i<A.dofs; i++) A[i][0] = fmax(A[i][0], B[0][0]);',
+         dx, {'A' : (I_cg_O1, RW), 'B': (I_O1, READ)} )
+
+# Heaviside step function in omega 2
+I_O2 = Function( V_DG0_O2 )
+par_loop( 'for ( int i=0; i < f.dofs; i++ ) f[i][0] = 1.;', dx(Ω2), {'f': (I_O2, WRITE)} )
+I_cg_O2 = Function(V)
+par_loop( ' for (int i=0; i<A.dofs; i++) for(int j=0; j<2; j++) A[i][j] = fmax(A[i][j], B[0][0]);',
+         dx, {'A' : (I_cg_O2, RW), 'B': (I_O2, READ)} )
+
+
+class MyBC(DirichletBC):
+    def __init__(self, V, value, markers):
+        # Call superclass init
+        # We provide a dummy subdomain id.
+        super(MyBC, self).__init__(V, value, 0)
+        # Override the "nodes" property which says where the boundary
+        # condition is to be applied.
+        self.nodes = np.unique(np.where(markers.dat.data_ro_with_halos == 0)[0])
+
+# magical B.C. generator to allow us not to have singular system
+BC_include_O2_only = MyBC(V,0,I_cg_O2)
+BC_include_O1_only = MyBC(V,0,I_cg_O1)
+
+
+
+a1 = (dot(grad(u), grad(v))) * dx(Ω1)
+L1 = f * v * dx(Ω1)
+a2 = (dot(grad(v), grad(u))) * dx(Ω2)
+L2 = f * v * dx(Ω2)
+
+u1=Function(V)
+u2=Function(V)
+u1.interpolate(0*x*y)
+u2.interpolate(0*x*y)
+
+a = (dot(grad(v), grad(u))) * dx
+L = f * v * dx
+u=Function(V)
+
+bc1=DirichletBC(V,0,1)
+bc2=DirichletBC(V,0,4)
+bc3=DirichletBC(V,0,5)
+bc4=DirichletBC(V,0,6)
+bc5=DirichletBC(V,0,7)
+bc6=DirichletBC(V,0,8)
+solve(a == L, u, bcs=[bc1,bc2,bc3,bc4,bc5,bc6])
+f.interpolate(sin(2*pi*x)*sin(2*pi*y))
+original_solution_error=sqrt(assemble(dot(u-f,u-f)*dx))
+print(original_solution_error)
+
+
+bc1=DirichletBC(V,0,1)
+bc2=DirichletBC(V,0,4)
+bc3=DirichletBC(V,0,5)
+bc4=DirichletBC(V,0,6)
+bc5=DirichletBC(V,0,7)
+bc6=DirichletBC(V,0,8)
+
+e1=u2
+e2=u1
+bc_dOmega1_n_Omega2=DirichletBC(V,e1,dΩ1nΩ2)
+bc_dOmega2_n_Omega1=DirichletBC(V,e2,dΩ2nΩ1)
+
+nSchwarzIter=100
+
+params={'ksp_type':'preonly','pc_type':'lu'}
+
+for iteration in range(nSchwarzIter):
+    solve(a1 == L1,u1,bcs=[bc1,bc2,bc3,bc4,bc5,bc6,bc_dOmega1_n_Omega2,BC_include_O1_only], solver_parameters=params)
+    solve(a2 == L2,u2,bcs=[bc1,bc2,bc3,bc4,bc5,bc6,bc_dOmega2_n_Omega1,BC_include_O2_only], solver_parameters=params)
+
+e1=assemble(dot(u1-f,u1-f)*dx(Ω1))
+e2=assemble(dot(u2-f,u2-f)*dx(Ω2))
+new_solution_error=sqrt(e1**2+e2**2)
+print(new_solution_error)
